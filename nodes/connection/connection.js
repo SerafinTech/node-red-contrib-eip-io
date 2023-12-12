f13f549d0c538a0a4fde7650020c103afff3d6cd
Jason Serafin <jtserafin@gmail.com>
2023-12-12 17:05:12 -0500

    updated config

--- nodes/connection/connection.js
+++ nodes/connection/connection.js
@@ -1,3 +1,5 @@
+const { off } = require("process");
+
 module.exports = function(RED) {
     function Connection(n) {
         RED.nodes.createNode(this,n);
@@ -20,6 +22,8 @@ module.exports = function(RED) {
         if (Array.isArray(n.configdata)) {
             let buf = Buffer.alloc(config.configInstance.size);
             let offset = 0;
+            let bitOffset = 0;
+            let bitString = null;
             n.configdata.forEach(data => {
                 let dataArray = JSON.parse(data.value)
                 if (Array.isArray(dataArray)) {
@@ -28,23 +32,64 @@ module.exports = function(RED) {
                         offset++;
                     }
                 } else {
-                    switch (parseInt(data.size)) {
-                        case 1:
-                            buf.writeUInt8(parseInt(data.value), offset);
-                            offset++;
-                            break;
-                        case 2:
-                            buf.writeUInt16LE(parseInt(data.value), offset);
-                            offset+= 2;
-                            break;
-                        case 4:
-                            buf.writeUInt32LE(parseInt(data.value), offset);
-                            offset+= 4;
-                            break;
+                    let size = Number(data.size);
+                    let multi = Math.pow(10, Number(data.decimals));
+                    let value = Number(data.value);
+                    let type = Number(data.type);
+                    console.log(data.type)
+                    if(size < 8) {
+                        if (bitString === null) bitString = 0;
+                        bitString = ((value * multi) << bitOffset) | bitString;
+                        bitOffset += size;
+
+                        if (bitOffset === 8) {
+                            buf.writeUInt8(bitString, offset)
+                            bitOffset = 0;
+                            bitString = null;
+                            offset++
+                        }
+                        if (bitOffset === 16) {
+                            buf.writeUInt16LE(bitString, offset)
+                            bitOffset = 0;
+                            bitString = null;
+                            offset += 2
+                        }
+                        if (bitOffset === 32) {
+                            buf.writeUInt32LE(bitString, offset)
+                            bitOffset = 0;
+                            bitString = null;
+                            offset += 4
+                        }    
+                    }
+                    if (size === 8) {
+                        if(type === 194) {
+                            buf.writeInt8(value * multi, offset);
+                        } else {
+                            buf.writeUInt8(value * multi, offset);
+                        }
+                        offset ++;
+                    }
+                    if (size === 16) {
+                        if(type === 195) {
+                            buf.writeInt16LE(value * multi, offset);
+                        } else {
+                            buf.writeUint16LE(value * multi, offset);
+                        }
+                        offset += 2;
+                    }
+                    if (size === 32) {
+                        if(type === 196) {
+                            buf.writeInt32LE(value * multi, offset);
+                        } else {
+                            buf.writeUint32LE(value * multi, offset);
+                        }
+                        offset += 4;
                     }
                 }
             })
+
             config.configInstance.data = buf;
+            console.log(config.configInstance.data)
         }
 
         this.scanner = RED.nodes.getNode(n.scanner).scanner;
